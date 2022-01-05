3be6a21fa4cfbae7dd44e8886b529d48efab3b28
jason-I-T <j.istejada29@gmail.com>
2022-01-05 08:41:38 -0800

    Started navbar

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
-    <h1 class="text-7xl underline">Is This Thing On?!</h1>
-    <Test />
   </div>
 </template>
 
 <script>
-import Test from './Test'
 
+// Create a nav bar
 export default {
   name: 'App',
   components: {
-    Test,
   }
 }
 </script>
