df302ebdab9d4e738984895f32a142e369b10be9
jason-I-T <j.istejada29@gmail.com>
2022-01-05 09:32:50 -0800

    Completed basic navbar

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
+    <Navbar />
   </div>
 </template>
 
 <script>
+import Navbar from './Navbar.vue'
 
 // Create a nav bar
 export default {
   name: 'App',
   components: {
+    Navbar,  
   }
 }
 </script>
--- vueapp/src/Navbar.vue
+++ vueapp/src/Navbar.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
+  <div class="bg-black text-5xl">
+    <ul class="flex justify-center">
+      <li class="m-5">
+        <a class="text-white hover:text-blue-800" href="#">TODO</a>
+      </li>
+      <li class="m-5">
+        <a class="text-white hover:text-blue-800" href="#">About</a>
+      </li>
+    </ul>
   </div>
 </template>
 
-<script></script>
+<script>
+  
+export default {
+  name: 'Navbar',
+}
+
+</script>
