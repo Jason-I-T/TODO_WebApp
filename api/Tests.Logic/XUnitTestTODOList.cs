88e5c96f889d338ad8bed9f4af93851f983a83c7
JasonTejadaRevature <jason263@revature.net>
2023-04-10 14:49:08 -0700

    Refactored testing directory, added TODOs for intended tests

---
+++ api/Tests.Logic/XUnitTestTODOList.cs
@@ -0,0 +1,12 @@
+namespace Tests.Logic;
+
+public class XUnitTestTODOList
+{
+    // TODO Test Show TODO List
+    
+
+    // TODO Test Add to TODO list
+    // TODO Test Delete from TODO list
+    // TODO Test Mark as Complete
+    // TODO Test Mark as Incomplete
+}
